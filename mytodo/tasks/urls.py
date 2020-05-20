c1f414fc865224df7b735cfb7723f33680024e6c
CharliesCodeFabrik <vincent-s@posteo.de>
2020-05-20 23:24:40 +0200

    todo seite. datenbank in liste und löschen button funktioniert

--- mytodo/tasks/urls.py
+++ mytodo/tasks/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('loeschen', views.loeschen, name='loeschen'),
     path('newtodo/', views.newtodo, name='newtodo'),
     path('newtodo/save', views.savenewtodo, name='savenewtodo'),
 ]
--- mytodo/tasks/views.py
+++ mytodo/tasks/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Todo
+from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    latest_todo_list = Todo.objects.order_by('id')[:20]
+    context = {'latest_todo_list': latest_todo_list}
+    return render(request, 'tasks/index.html', context)
 
 
 def newtodo(request):
@@ -19,3 +26,12 @@ def savenewtodo(request):
     )
     return HttpResponse("Speichern war erfolgreich.")
 
+
+def loeschen(request):
+    todos = Todo.objects.all()
+    for o in todos:
+        if str(o.id) in request.POST:
+            o.delete()
+    latest_todo_list = Todo.objects.order_by('id')[:20]
+    context = {'latest_todo_list': latest_todo_list}
+    return render(request, 'tasks/index.html', context)
